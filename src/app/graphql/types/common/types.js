74a0f81c6dfed3d8692e6fd8192ffe73ae0c1cf8
Helen Dias <helendias.hd@gmail.com>
2020-05-01 16:46:32 -0300

    :bug: Fix common types

--- src/app/graphql/types/common/types.js
+++ src/app/graphql/types/common/types.js
@@ -4,15 +4,33 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
   """Type that represents a DateTime data"""
   scalar DateTime
-  
+
   type RangeDate {
     start: DateTime
     end: DateTime
   }
-  
-  input InputRangeDate {
+
+  input RangeDateInput {
     start: String
     end: String
+  }
+
+  type Location {
+    country: String
+    state: String
+    city: String
+    neighborhood: String
+    street: String
+    number: String
+  }
+
+  input LocationInput {
+    country: String
+    state: String
+    city: String
+    neighborhood: String
+    street: String
+    number: String
   }
 `;
 
