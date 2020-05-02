d9553a504054159c70efb64c8a8c5c14a301a34e
Helen Dias <helendias.hd@gmail.com>
2020-05-02 16:16:32 -0300

    :rotating_light: Fix lint errors

--- src/app/graphql/schema.js
+++ src/app/graphql/schema.js
@@ -2,11 +2,11 @@ const { merge, flatten } = require('lodash');
 const { makeExecutableSchema } = require('graphql-tools');
 const { gql } = require('apollo-server-express');
 
-const { userTypeDefs, userResolvers } = require('./types/user/');
-const { signTypeDefs, signResolvers } = require('./types/sign/');
-const { storeTypeDefs, storeResolvers } = require('./types/store/');
-const { commonTypeDefs, commonResolvers } = require('./types/common/');
-const { sysActionTypeDefs, sysActionResolvers } = require('./types/sysAction/');
+const { userTypeDefs, userResolvers } = require('./types/user');
+const { signTypeDefs, signResolvers } = require('./types/sign');
+const { storeTypeDefs, storeResolvers } = require('./types/store');
+const { commonTypeDefs, commonResolvers } = require('./types/common');
+const { sysActionTypeDefs, sysActionResolvers } = require('./types/sysAction');
 
 // DEFAULT EMPTY ROOT TYPES
 const RootTypes = gql`
