feccfdf30f450eff51187d22411ffe26a8fd6b40
Helen Dias <helendias.hd@gmail.com>
2020-05-02 14:16:15 -0300

    :building_construction: Add SysAction mongo schema

---
+++ src/app/domain/infrastructure/mongodb-models/SysActionPersistentModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const sysActionMongoSchema = new Schema({
+  requester: { type: Schema.Types.ObjectId, ref: 'users' },
+  hash: { type: String, required: true },
+  action: {
+    type: String,
+    required: true,
+    uppercase: true,
+    enum: [
+      'FORGOT_PASS',
+      'FINISH_ACCOUNT',
+      'INVITE_USER',
+    ],
+  },
+  metadata: { type: String, required: false },
+  used: { type: Boolean, default: false },
+  expirationDate: { type: Date, required: true },
+  creationDate: { type: Date, default: Date.now },
+  active: { type: Boolean, default: true },
+});
+module.exports = mongoose.model('sysActions', sysActionMongoSchema);
