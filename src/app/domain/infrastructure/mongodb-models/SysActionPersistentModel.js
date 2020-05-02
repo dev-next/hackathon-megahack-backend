09284d4aad209334162804cc1c3efcefd1b784f6
Helen Dias <helendias.hd@gmail.com>
2020-05-02 17:54:02 -0300

    :bug: Fix sys action mongo schema

--- src/app/domain/infrastructure/mongodb-models/SysActionPersistentModel.js
+++ src/app/domain/infrastructure/mongodb-models/SysActionPersistentModel.js
@@ -1,22 +1,28 @@
-const mongoose = require('mongoose');
+function GenerateUsersMongoSchema(injection) {
+  const {
+    connection,
+    Schema,
+  } = Object.assign({}, injection);
+  const userMongoSchema = new Schema({
+    requester: { type: Schema.Types.ObjectId, ref: 'users' },
+    hash: { type: String, required: true },
+    action: {
+      type: String,
+      required: true,
+      uppercase: true,
+      enum: [
+        'FORGOT_PASS',
+        'FINISH_ACCOUNT',
+        'INVITE_USER',
+      ],
+    },
+    metadata: { type: String, required: false },
+    expirationDate: { type: Date, required: true },
+    creationDate: { type: Date, default: Date.now },
+    active: { type: Boolean, default: true },
+  }, { timestamps: { createdAt: 'creationDate', updatedAt: 'updateDate' } });
 
-const { Schema } = mongoose;
-const sysActionMongoSchema = new Schema({
-  requester: { type: Schema.Types.ObjectId, ref: 'users' },
-  hash: { type: String, required: true },
-  action: {
-    type: String,
-    required: true,
-    uppercase: true,
-    enum: [
-      'FORGOT_PASS',
-      'FINISH_ACCOUNT',
-      'INVITE_USER',
-    ],
-  },
-  metadata: { type: String, required: false },
-  expirationDate: { type: Date, required: true },
-  creationDate: { type: Date, default: Date.now },
-  active: { type: Boolean, default: true },
-});
-module.exports = mongoose.model('sysActions', sysActionMongoSchema);
+  return connection.model('sysActions', userMongoSchema);
+}
+
+module.exports = injection => GenerateUsersMongoSchema(injection);
