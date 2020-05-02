283427f9d748866b01ffb68727ffd33c72139c93
Helen Dias <helendias.hd@gmail.com>
2020-05-01 22:23:16 -0300

    :lock: Add cryptography on create user

--- src/app/domain/use-cases/user/create-store-and-owner/CreateStoreAndOwner.js
+++ src/app/domain/use-cases/user/create-store-and-owner/CreateStoreAndOwner.js
@@ -2,6 +2,7 @@ const dependencies = {
   UserRepository: require('../../../infrastructure/repository/UserRepository'),
   StoreRepository: require('../../../infrastructure/repository/StoreRepository'),
   CreatePassword: require('../../../../utils/CreatePassword'),
+  bcrypt: require('bcrypt'),
 };
 
 const CreateStoreAndOwner = async (data, injection) => {
@@ -11,16 +12,20 @@ const CreateStoreAndOwner = async (data, injection) => {
     StoreRepository,
     UserRepository,
     Logger,
+    bcrypt,
   } = Object.assign({}, dependencies, injection);
 
-  console.log(injection);
   try {
     const store = await new StoreRepository(injection, StorePersistentModel).create(data.store);
+
+    const salt = await bcrypt.genSalt();
+    const password = await bcrypt.hash(data.user.password, salt);
 
     return new UserRepository(injection, UserPersistentModel).create({
       ...data.user,
       type: 'STORE_OWNER',
       stores: [store._id],
+      password,
     });
   } catch (e) {
     Logger.warn(e.message);
