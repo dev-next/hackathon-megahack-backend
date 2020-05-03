180588c472529e56c74fe4e7547448169e1382af
Helen Dias <helendias.hd@gmail.com>
2020-05-02 23:51:33 -0300

    :art: Change structure of Catalogue mongo schema

--- src/app/domain/infrastructure/mongodb-models/CataloguePersistentModel.js
+++ src/app/domain/infrastructure/mongodb-models/CataloguePersistentModel.js
@@ -6,14 +6,26 @@ function GenerateCalalogueMongoSchema(injection) {
   const catalogueMongoSchema = new Schema({
     name: { type: String, required: true },
     description: String,
-    photos: [String],
-    value: Number,
-    tags: [String],
-    fields: [{
-      label: String,
-      value: String,
-    }],
+    slug: {
+      type: String,
+      required: true,
+      lowercase: true,
+      index: { unique: true },
+    },
+    seller: { type: Schema.Types.ObjectId, ref: 'users' },
+    customer: { type: Schema.Types.ObjectId, ref: 'customer' },
     store: { type: Schema.Types.ObjectId, ref: 'stores' },
+    items: [{
+      name: { type: String, required: true },
+      description: String,
+      photos: [String],
+      value: Number,
+      tags: [String],
+      fields: [{
+        label: String,
+        value: String,
+      }],
+    }],
     createdBy: { type: Schema.Types.ObjectId, ref: 'users' },
     active: { type: Boolean, default: true },
   }, { timestamps: { createdAt: 'creationDate', updatedAt: 'updateDate' } });
