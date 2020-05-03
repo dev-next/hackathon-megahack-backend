9a8b9bcdf41d4e78610cbeb74f0e5d53f4cbf3f0
Helen Dias <helendias.hd@gmail.com>
2020-05-03 09:45:00 -0300

    :sparkles: Add mongo schema of Order

---
+++ src/app/domain/infrastructure/mongodb-models/OrderPersistentModel.js
@@ -0,0 +1,60 @@
+function GenerateOrdersMongoSchema(injection) {
+  const {
+    connection,
+    Schema,
+  } = Object.assign({}, injection);
+  const orderMongoSchema = new Schema({
+    name: { type: String, required: true },
+    number: Number,
+    value: Number,
+    store: { type: Schema.Types.ObjectId, ref: 'stores' },
+    delivery: {
+      location: {
+        country: String,
+        state: String,
+        city: String,
+        number: String,
+        neighborhood: String,
+        zipcode: String,
+      },
+      type: {
+        type: String,
+        uppercase: true,
+        default: 'STORE',
+        enum: ['HOME', 'STORE'],
+      },
+      dates: Date,
+      hours: [{
+        type: String,
+        uppercase: true,
+        default: 'MORNING',
+        enum: ['MORNING', 'AFTERNOON']
+      }],
+      shipment: Number,
+    },
+    payment: {
+      method: {
+        type: String,
+        uppercase: true,
+        default: 'MONEY',
+        enum: ['MONEY', 'CREDIT_CARD'],
+      },
+      needChange: Boolean,
+    },
+    items: [{
+      name: String,
+      value: Number,
+      description: String,
+      fields: [{
+        label: String,
+        value: String,
+      }],
+    }],
+    customer: { type: Schema.Types.ObjectId, ref: 'users' },
+    seller: { type: Schema.Types.ObjectId, ref: 'users' },
+    active: { type: Boolean, default: true },
+  }, { timestamps: { createdAt: 'creationDate', updatedAt: 'updateDate' } });
+  return connection.model('orders', orderMongoSchema);
+}
+
+module.exports = injection => GenerateOrdersMongoSchema(injection);
